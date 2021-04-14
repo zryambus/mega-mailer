ae0e7f4b8dc32fa6a694db92af01bfc92eeecb8d
Ilya Volchenkov <ivolchenkov@megaputer.ru>
2021-04-14 11:29:52 +0300

    Fixed build and publish script
    Added building of ui docker image

--- build-and-push.fish
+++ build-and-push.fish
@@ -4,6 +4,10 @@ cd mega-mailer-src
 ./build.fish
 cd ..
 
+cd mega-mailer-ui
+docker build -t mega-mailer-ui .
+cd ..
+
 docker build -t ivolchenkov/mega-mailer-mail-checker -f docker/mail_checker.docker .
 docker build -t ivolchenkov/mega-mailer-telegram-bot -f docker/telegram_bot.docker .
 docker build -t ivolchenkov/mega-mailer-web-server -f docker/web_server.docker .
